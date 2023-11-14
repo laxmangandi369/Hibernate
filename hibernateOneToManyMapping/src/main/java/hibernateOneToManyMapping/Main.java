8be9100d7d7645dd30bc9f5a72f5d1098320a651
laxmangandi369 <119059799+laxmangandi369@users.noreply.github.com>
2023-11-14 19:19:19 +0530

    Update Main.java

--- hibernateOneToManyMapping/src/main/java/hibernateOneToManyMapping/Main.java
+++ hibernateOneToManyMapping/src/main/java/hibernateOneToManyMapping/Main.java
@@ -18,6 +18,38 @@ public class Main {
 		Passenger p3=new Passenger("Anand", "8668530412");
 		Passenger p4=new Passenger("Atif", "8668530413");
 		Passenger p5=new Passenger("Monis", "9668530410");
+
+		Passenger p1=new Passenger("Rohan", "8668530410");
+		Passenger p2=new Passenger("Abhinav", "8668530411");
+		Passenger p3=new Passenger("Anand", "8668530412");
+		Passenger p4=new Passenger("Atif", "8668530413");
+		Passenger p5=new Passenger("Monis", "9668530410");
+
+		Passenger p1=new Passenger("Rohan", "8668530410");
+		Passenger p2=new Passenger("Abhinav", "8668530411");
+		Passenger p3=new Passenger("Anand", "8668530412");
+		Passenger p4=new Passenger("Atif", "8668530413");
+		Passenger p5=new Passenger("Monis", "9668530410");
+
+
+		Passenger p1=new Passenger("Rohan", "8668530410");
+		Passenger p2=new Passenger("Abhinav", "8668530411");
+		Passenger p3=new Passenger("Anand", "8668530412");
+		Passenger p4=new Passenger("Atif", "8668530413");
+		Passenger p5=new Passenger("Monis", "9668530410");
+
+
+		Passenger p1=new Passenger("Rohan", "8668530410");
+		Passenger p2=new Passenger("Abhinav", "8668530411");
+		Passenger p3=new Passenger("Anand", "8668530412");
+		Passenger p4=new Passenger("Atif", "8668530413");
+		Passenger p5=new Passenger("Monis", "9668530410");
+
+		Passenger p1=new Passenger("Rohan", "8668530410");
+		Passenger p2=new Passenger("Abhinav", "8668530411");
+		Passenger p3=new Passenger("Anand", "8668530412");
+		Passenger p4=new Passenger("Atif", "8668530413");
+		Passenger p5=new Passenger("Monis", "9668530410");
 		Set<Passenger> bus1passengerdetails = new HashSet<Passenger>();
 		Set<Passenger> bus2passengerdetails = new HashSet<Passenger>();
 		bus1passengerdetails.add(p1);
